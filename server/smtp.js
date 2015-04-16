1ed025fbef918ed59530e25bc045a3f8c8d0c49d
Ciurtatul <razvan_film@yahoo.com>
2015-04-16 00:07:00 +0000

    Implemented email notifications

--- server/smtp.js
+++ server/smtp.js
@@ -2,52 +2,65 @@ Meteor.startup(function () {
 });
 
 
-validateUser = function (authorizedSender) {
+validateUser = function (user) {
   var errors = {};
-  var currentUser = Meteor.user();
 
-  if (currentUser.username != authorizedSender) {
-    errors.description = "This user (" + currentUser.username + ") is not authorized to send emails";
+  console.log("Email sender: " + user);
+
+  if(!Meteor.settings.SMTPauthorizedSenders)
+    throw new Meteor.Error(500, 'Please provide SMTPauthorizedSenders in Meteor.settings');
+  var authorizedSenders = Meteor.settings.SMTPauthorizedSenders;
+
+  if (authorizedSenders.every(function(item){
+        return item != user;
+      })) {
+    errors.description = "This user (" + user + ") is not authorized to send emails";
   }
   return errors;
 }
 
 
 Meteor.methods({
-  /*//1:*returnUserFields: function (userId) {
-    check(userId, String);
-    //var user = Meteor.users.findOne(userId);
-    var user = Meteor.users.find();
-    return user;
-  },//1:*/
-  sendEmail: function (from, to, subject, text) {
-    check([from, to, subject, text], [String]);
-    //check(to, [String]);
+
+  sendEmail: function (to, subject, html) {
+
+    //check([from, to, subject, text], [String]);
+    check([subject, html], [String]);
+    check(to, [String]);
+
+
+    if(!Meteor.settings.SMTPSenderAddress)
+      throw new Meteor.Error(500, 'Please provide SMTPSenderAddress in Meteor.settings');
+    var from = Meteor.settings.SMTPSenderAddress;
+
+    if(!Meteor.settings.SMTPReceiverAll)
+      throw new Meteor.Error(500, 'Please provide SMTPReceiverAll in Meteor.settings');
+    var receiverAll = Meteor.settings.SMTPReceiverAll;
     
     if(!Meteor.settings.SMTPMailUrl)
       throw new Meteor.Error(500, 'Please provide a SMTP Mail URL in Meteor.settings');
     process.env.MAIL_URL = Meteor.settings.SMTPMailUrl; 
     
-    if(!Meteor.settings.SMTPauthorizedSender)
-      throw new Meteor.Error(500, 'Please provide SMTPauthorizedSender in Meteor.settings');
-      
-    var authorizedSender = Meteor.settings.SMTPauthorizedSender;
 
 
 
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
     this.unblock();
-    
-    var errors = validateUser(authorizedSender);
+
+    var errors = validateUser(Meteor.user().username);
     if (errors.description)
       throw new Meteor.Error('not-authorized', errors.description);
+
+    //to.push(receiverAll);
+    //console.log("Recipients: " + to);
     
     Email.send({
       from: from,
-      to: to,      
+      to: to,
+      bcc: receiverAll,
       subject: subject,
-      text: text,
+      html: html,
     });
     
   }
